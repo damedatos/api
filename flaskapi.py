c2bf6dccad3961c6e431a2169e7c0361ee57e0ef
freire-guido <14301656+freire-guido@users.noreply.github.com>
2023-11-12 17:08:19 -0300

    id -> materia

--- flaskapi.py
+++ flaskapi.py
@@ -11,8 +11,14 @@ app.wsgi_app = ProxyFix(
 with open('materias.json', 'r') as json_file:
     materias = json.load(json_file)
 
+def materiaPorID(id):
+    for materia in materias:
+        if materia['id'] == id:
+            return materia
+    return None
+
 def materiasPorIDs(ids):
-    results = [materia for materia in materias if materia['id'] in ids]
+    results = [materiaPorID(id) for id in ids]
     return results
 
 @app.route('/api/materias/buscar', methods=['GET'])
@@ -24,7 +30,8 @@ def buscar():
 @app.route('/api/materias/recomendar', methods=['POST'])
 def recomendar():
     data = request.get_json()
-    return materiasPorIDs(recomendador(data['materias']))
+    recs = recomendador(data['materias'])
+    return materiasPorIDs(recs[:min(6, len(recs))])
 
 @app.route('/api/log', methods=['POST'])
 def logger():
